6de646622f07357849a1ab52e18637e877b1f18e
Saeki, Takahiro <ts-takahiro.saeki@rakuten.com>
2018-05-15 10:40:15 +0900

    first awesome commit.

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    id: 1,
+    title: 'mock1',
+    type: 1
+  },
+  {
+    id: 2,
+    title: 'mock2',
+    type: 1
+  },
+  {
+    id: 3,
+    title: 'mock3',
+    type: 2
+  },
+  {
+    id: 4,
+    title: 'mock4',
+    type: 2
+  },
+  {
+    id: 5,
+    title: 'mock5',
+    type: 2
+  }
+]
+
+export default data;
